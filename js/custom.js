88f40ac013e691f584f4f654d21112fd61cf4fc8
Gaurav Goyal <gaurav.goyal@innoraft.com>
2016-06-23 21:36:27 +0530

    Menu completed

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+showMenu = function() {
+	document.getElementById("show-navigation").style.display = 'block';
+	document.getElementById('showMenu').style.display = 'none';
+};
+
+hideMenu = function() {
+	document.getElementById("show-navigation").style.display = 'none';
+	document.getElementById('showMenu').style.display = 'block';
+};
