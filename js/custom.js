ddb867401e6f2f88424f6fb9baa4842473aa864c
Gaurav Goyal <gaurav.goyal@innoraft.com>
2016-10-01 03:26:51 +0530

    Removed all previous blog files

--- js/custom.js
+++
@@ -1,9 +0,0 @@
-showMenu = function() {
-	document.getElementById("show-navigation").style.display = 'block';
-	document.getElementById('showMenu').style.display = 'none';
-};
-
-hideMenu = function() {
-	document.getElementById("show-navigation").style.display = 'none';
-	document.getElementById('showMenu').style.display = 'block';
-};
